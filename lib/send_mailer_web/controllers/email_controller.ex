7c798e65a9056d81387eddfc163d2094a2d48e91
mikeDRei <michaelalvesreino@outlook.com>
2022-06-24 16:02:51 -0300

    create controller send_mailer

---
+++ lib/send_mailer_web/controllers/email_controller.ex
@@ -0,0 +1,41 @@
+defmodule SendMailerWeb.EmailController do
+  use SendMailerWeb, :controller
+
+  alias SendMailer.Email
+  alias SendMailer.Service.EmailServer
+  alias SendMailer.Email.SendEmail
+
+  action_fallback SendMailerWeb.FallbackController
+
+
+  def send_email(conn, %{"send_email" => email_params}) do
+    try do
+      send_email =  Email.SendEmail.changeset(%SendEmail{}, email_params).changes
+      
+      if(SendEmail.changeset(%SendEmail{}, email_params).valid?) do
+        EmailServer.send_email_from(send_email)
+        IO.inspect send_email
+        conn
+        |> put_status(:ok)
+        |> json(%{
+          message: "Email successfully sent!",
+          status: 200,
+          email_content: send_email
+        })
+      else
+        conn
+        |> put_status(:bad_request)
+        |> json(%{
+          message: "Error sending email, check the parameters informed.",
+          status: 400,
+          params: email_params
+        })
+      end
+      rescue
+        e ->
+          conn
+          |> put_status(:internal_server_error)
+          |> json(e.message)
+    end
+  end
+end
