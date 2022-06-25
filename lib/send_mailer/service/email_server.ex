1ce187c1f00a42627024bb45a02aad9699c50098
mikeDRei <michaelalvesreino@outlook.com>
2022-06-25 19:20:33 -0300

    update email_server

--- lib/send_mailer/service/email_server.ex
+++ lib/send_mailer/service/email_server.ex
@@ -1,7 +1,7 @@
 defmodule SendMailer.Service.EmailServer do
   use Bamboo.Phoenix, view: SendMailerWeb.EmailView
   use Bamboo.Mailer, otp_app: :send_mailer
-# email_args = %{"attachment_links" => ["asasas"], "bcc" => ["michaalvesreino@gmail.com"], "cc" => ["michaalvesreino@gmail.com"], "content" => "<h2>asasas</h2>", "email_name" => "test", "from" => "michaalvesreino@gmail.com", "other_values" => %{"exam-core" => "454545"}, "subject" => "Welcome!!", "to" => ["michaelalvesreino@gmail.com"]}
+
   import Bamboo.Email
 
   def send_email_from(email_args) do
