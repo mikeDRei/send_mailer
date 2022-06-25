e7f56e7bc8d035f1122b76f26aaff69b35fa4f29
mikeDRei <michaelalvesreino@outlook.com>
2022-06-25 17:29:06 -0300

    create email_view.ex

---
+++ lib/send_mailer_web/views/email_view.ex
@@ -0,0 +1,27 @@
+defmodule SendMailerWeb.EmailView do
+  use SendMailerWeb, :view
+  alias SendMailerWeb.EmailView
+  # email_args = %{"attachment_links" => ["asasas"], "bcc" => ["michaalvesreino@gmail.com"], "cc" => ["michaalvesreino@gmail.com"], "content" => "<h2>asasas</h2>", "email_name" => "test", "from" => "michaalvesreino@gmail.com", "other_values" => %{"exam-core" => "454545"}, "subject" => "Welcome!!", "to" => ["michaelalvesreino@gmail.com"]}
+
+  def render("index.json", %{email_args: email_args}) do
+    %{data: render_many(email_args, EmailView, "email_args.json")}
+  end
+
+  def render("show.json", %{email_args: email_args}) do
+    %{data: render_one(email_args, EmailView, "email_args.json")}
+  end
+
+  def render("email_args.json", %{email_args: email_args}) do
+    %{
+      from: email_args.cc,
+      cc: email_args.cc,
+      bcc: email_args.bcc,
+      to: email_args.to,
+      subject: email_args.subject,
+      content: email_args.content,
+      attachment_links: email_args.attachment_links,
+      other_values: email_args.other_values,
+      email_name: email_args.email_name
+    }
+  end
+end
