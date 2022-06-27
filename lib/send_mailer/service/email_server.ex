90c8641c8d9988322356c73885f3e105bbdd6a40
mikeDRei <michaelalvesreino@outlook.com>
2022-06-27 15:25:53 -0300

    root path attachment

--- lib/send_mailer/service/email_server.ex
+++ lib/send_mailer/service/email_server.ex
@@ -8,15 +8,20 @@ defmodule SendMailer.Service.EmailServer do
     try do
       new_email()
       |> from(email_args["from"])
-      |> cc(email_args["cc"])
+      |> bcc(email_args["cc"])
       |> to(email_args["to"])
       |> subject(email_args["subject"])
       |> assign(:email_args, email_args)
+      # |> put_attachment(Bamboo.Attachment.new(root_path(), filename: "logotipo.png", content_type: "image/png", content_id: "logotipo.png"))
       |> render(email_args["email_name"] <> ".html")
       |> deliver_now()
     rescue
-      e->
-        raise "incorrect parameter error"
+      e ->
+        raise "incorrect parameter error #{e.message}"
     end
+  end
+
+  def root_path() do
+    Path.absname("lib/send_mailer/service/images/logotipoRosa.png")
   end
 end
