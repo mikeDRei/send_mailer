d671c1c0357a1a8554379cac1967ff45f241cbad
mikeDRei <michaelalvesreino@outlook.com>
2022-06-28 15:43:12 -0300

    adding environment variable test.exs

--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :send_mailer, SendMailer.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  hostname: System.get_env("DB_HOSTNAME"),
   database: "send_mailer_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
