6793adf39bec361129fa3d2218c5a9800fb72321
mikeDRei <michaelalvesreino@outlook.com>
2022-06-28 14:24:16 -0300

    adding :from value

--- lib/send_mailer/service/email_server.ex
+++ lib/send_mailer/service/email_server.ex
@@ -7,12 +7,13 @@ defmodule SendMailer.Service.EmailServer do
   def send_email_from(email_args) do
     try do
       new_email()
-      |> from(email_args["from"])
+      |> from("michaalvesreino@gmail.com")
       |> bcc(email_args["cc"])
       |> to(email_args["to"])
       |> subject(email_args["subject"])
       |> assign(:email_args, email_args)
-      # |> put_attachment(Bamboo.Attachment.new(root_path(), filename: "logotipo.png", content_type: "image/png", content_id: "logotipo.png"))
+      # |> put_attachment(Bamboo.Attachment.new(root_path(), filename: "logotipo.png",
+        #content_type: "image/png", content_id: "logotipo.png"))
       |> render(email_args["email_name"] <> ".html")
       |> deliver_now()
     rescue
