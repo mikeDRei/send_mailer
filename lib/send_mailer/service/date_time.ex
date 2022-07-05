7b0d2550cd9c1c9c757398817ec333cea459df63
mikeDRei <michaelalvesreino@outlook.com>
2022-07-05 17:31:57 -0300

    update date_time

--- lib/send_mailer/service/date_time.ex
+++ lib/send_mailer/service/date_time.ex
@@ -1,6 +1,6 @@
 defmodule SendMailer.Service.DateTime do
   def dateTime() do
     utc = DateTime.now!("America/Sao_Paulo")
-    "[#{utc.hour}:#{utc.minute}:#{utc.second} #{utc.day}/#{utc.month}/#{utc.year}]"
+    "#{utc.hour}:#{utc.minute} #{utc.day}/#{utc.month}/#{utc.year}"
   end
 end
